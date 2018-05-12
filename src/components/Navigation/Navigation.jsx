5c35f4feafce21de605e2f691dcd7d4149a3dcd2
Dylan Sather <dylan.sather@gmail.com>
2018-05-12 12:28:42 -0700

    Changing nav bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Link from 'gatsby-link';
 import Headroom from 'react-headroom';
-import { FaInstagram, FaBehance, FaDribbble } from 'react-icons/lib/fa';
-import { slide as Menu } from 'react-burger-menu';
-import { Fade } from 'react-reveal';
+import {FaGithub, FaLinkedinSquare} from 'react-icons/lib/fa';
+import {slide as Menu} from 'react-burger-menu';
+import {Fade} from 'react-reveal';
 import config from '../../../config/SiteConfig';
 import styles from './Navigation.module.scss';
 import './Headroom.scss';
@@ -18,11 +18,11 @@ export default class Navigation extends Component {
   }
 
   handleStateChange(state) {
-    this.setState({ menuOpen: state.isOpen });
+    this.setState({menuOpen: state.isOpen});
   }
 
   closeMenu() {
-    this.setState({ menuOpen: false });
+    this.setState({menuOpen: false});
   }
   render() {
     return (
@@ -37,6 +37,12 @@ export default class Navigation extends Component {
                 <Link to="/contact" activeClassName="active">
                   Contact
                 </Link>
+                <a
+                  href="dylan-sather-resume.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Resume
+                </a>
               </span>
             </nav>
             <div className={styles.name}>
@@ -48,14 +54,17 @@ export default class Navigation extends Component {
             </div>
             <div className={styles.socialMedia}>
               <span>
-                <a href="https://www.instagram.com/lekoarts.de" target="_blank" rel="noopener noreferrer">
-                  <FaInstagram />
+                <a
+                  href="https://www.linkedin.com/in/dylansather/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <FaLinkedinSquare />
                 </a>
-                <a href="https://www.behance.net/lekoarts" target="_blank" rel="noopener noreferrer">
-                  <FaBehance />
-                </a>
-                <a href="https://dribbble.com/LeKoArts" target="_blank" rel="noopener noreferrer">
-                  <FaDribbble />
+                <a
+                  href="https://github.com/dylburger"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <FaGithub />
                 </a>
               </span>
             </div>
@@ -66,25 +75,43 @@ export default class Navigation extends Component {
             <h3>{config.siteTitle}</h3>
           </div>
           <div className={styles.menu}>
-            <Menu isOpen={this.state.menuOpen} onStateChange={this.handleStateChange} width="100%">
+            <Menu
+              isOpen={this.state.menuOpen}
+              onStateChange={this.handleStateChange}
+              width="100%">
               <Link to="/" onClick={() => this.closeMenu()}>
                 <h1>{config.siteTitle}</h1>
               </Link>
-              <Link to="/about" activeClassName="active" onClick={() => this.closeMenu()}>
+              <Link
+                to="/about"
+                activeClassName="active"
+                onClick={() => this.closeMenu()}>
                 About
               </Link>
-              <Link to="/contact" activeClassName="active" onClick={() => this.closeMenu()}>
+              <Link
+                to="/contact"
+                activeClassName="active"
+                onClick={() => this.closeMenu()}>
                 Contact
               </Link>
+              <a
+                href="dylan-sather-resume.pdf"
+                target="_blank"
+                rel="noopener noreferrer">
+                Resume
+              </a>
               <div className={styles.mobileNavSocialMedia}>
-                <a href="https://www.instagram.com/lekoarts.de" target="_blank" rel="noopener noreferrer">
-                  <FaInstagram />
+                <a
+                  href="https://www.linkedin.com/in/dylansather/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <FaLinkedinSquare />
                 </a>
-                <a href="https://www.behance.net/lekoarts" target="_blank" rel="noopener noreferrer">
-                  <FaBehance />
-                </a>
-                <a href="https://dribbble.com/LeKoArts" target="_blank" rel="noopener noreferrer">
-                  <FaDribbble />
+                <a
+                  href="https://github.com/dylburger"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <FaGithub />
                 </a>
               </div>
             </Menu>
